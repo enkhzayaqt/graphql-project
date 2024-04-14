a0d69989208b92d35a8da07473099020b582879f
enkhzayaqt <jinizaya@gmail.com>
2024-04-14 11:35:22 -0500

    setup token

--- server/authentication.js
+++ server/authentication.js
@@ -1,4 +1,17 @@
-export function handleLogin(req, res){
+import {getUserByEmail} from "./controllers/user"
+import jwt from 'jsonwebtoken'
+
+const secret = 'sdfgakjgsfpoqhguovsdgdvkj74972halhf'
+
+export async function handleLogin(req, res){
+    const {email, password} = req.body;
+    const user = await getUserByEmail(email);
+    if(!user || user.password !== password){
+        return res.sendStatus(401) // Unauthorized response status 
+    }
+    const data =  {sub: user.id, email: user.email};
+    const token = jwt.sign(data, secret);
+
     console.log(req.body);
-    res.json({token: "Hello"})
+    res.json({token: token})
 }
---
+++ server/controllers/user.js
@@ -0,0 +1,5 @@
+import knex from "../lib/db.js";
+
+export async function getUserByEmail(email){
+   return await knex.table('user').first().where({email:email})
+}
