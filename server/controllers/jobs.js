7649ac3d3867e2f979ab3e17db04e023d92efae1
enkhzayaqt <jinizaya@gmail.com>
2024-05-02 22:10:48 -0500

    fixed company resolver

--- server/controllers/jobs.js
+++ server/controllers/jobs.js
@@ -3,3 +3,7 @@ import knex from "../lib/db.js";
 export async function getJobs(){
     return await knex.table('job')
 }
+
+export async function getJobById(id){
+    return await knex.table('job').first().where({id})
+}
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,4 +1,4 @@
-import { getJobs } from "./controllers/jobs.js";
+import { getJobs, getJobById} from "./controllers/jobs.js";
 import { getCompanyById } from "./controllers/company.js";
 
 export const resolvers = {
@@ -6,6 +6,14 @@ export const resolvers = {
         jobs: async () => {
             const jobs = await getJobs();
             return jobs;
+        },
+        job: async (root, {id})=> {
+            const job = await getJobById(id);
+            return job;
+        },
+        company: async (root, {id}) => {
+            const company = await getCompanyById(id);
+            return company;
         }
     },
 
