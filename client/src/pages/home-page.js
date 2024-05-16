f4ec07bf789a798ce999c4fe98dbc3e804f950b6
enkhzayaqt <jinizaya@gmail.com>
2024-05-15 20:51:40 -0500

    getting jobs from db

--- client/src/pages/home-page.js
+++ client/src/pages/home-page.js
@@ -1,10 +1,16 @@
+import { useEffect, useState } from "react";
 import JobList from "../components/job-list"
 import { getJobs } from "../graphql/query";
-import {jobs} from "../lib/data"
 
-getJobs().then(data => console.log(data[0]))
 
 export default function HomePage(){
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(()=>{
+    getJobs().then(data => setJobs(data))
+  }, [])
+  
+  console.log(jobs)
     return (
       <>
         <div className="title">Job openings</div>
